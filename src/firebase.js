fe4c6247d574f1c21f8f9d131c9e50290215f690
Melina Scarfone <melinasolscarfone@gmail.com>
2021-12-24 11:09:36 -0300

    proyecto final

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,3 @@
-// import firebase from "firebase/app";
-// import "firebase/firestore"
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCYPDwLVXfUtCL33ESlvpe9_DvH0pbj83U",
-//   authDomain: "proyecto-react-2bcc5.firebaseapp.com",
-//   projectId: "proyecto-react-2bcc5",
-//   storageBucket: "proyecto-react-2bcc5.appspot.com",
-//   messagingSenderId: "98913946360",
-//   appId: "1:98913946360:web:156168bc882c333825490d"
-// };
-
-// const app = firebase.initializeApp(firebaseConfig);
-
-// export const firestore = firebase.firestore(app)
 import firebase from "firebase/app"
 import "firebase/firestore"
 
@@ -25,6 +10,5 @@ const firebaseConfig = {
   appId: "1:122240213164:web:4235d0c34bf044cf8b7f09"
 };
 
-// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 export const firestore = firebase.firestore(app)
